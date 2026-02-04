696d40db2164bfd20069c018adfd02ee11f1c6eb
IDontHaveABrain <josephleow05@gmail.com>
2026-02-04 11:27:52 +0800

    Update streamlit_app.py
    Changed title and added a short description

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("US House price predictor")
 st.write(
-    "Let's start building!!!! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Want to know the price of your dream house? Try out our AI to help give you a good estimate!"
 )
